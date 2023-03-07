c0298c2cc6e6bee2bfc2a7911e64630be7b8751d
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-03-07 22:15:38 +0100

    Data parsing Ryainair

--- src/Ryanair_Scrape.py
+++ src/Ryanair_Scrape.py
@@ -18,7 +18,8 @@ def get_data(arive, dates) -> dict:
         for origin in DEPART:
             for date in dates:
                 print(f"Pulling {origin} to {place} on {date}")
-                page = requests.get(URL.format(date=date, place=place, origin=origin))
+                page = requests.get(URL.format(
+                    date=date, place=place, origin=origin))
                 soup = BeautifulSoup(page.content, "lxml")
                 result = soup.find("p").text
                 json_reponse = json.loads(result)
@@ -28,15 +29,24 @@ def get_data(arive, dates) -> dict:
                         break
                     out = {}
                     try:
-                        out['destination'] = flight['destinationName']
-                        out['depart'] = flight['originName']
-                        out['date'] = datetime.fromisoformat(flight['dates'][0]['dateOut']).strftime("%Y-%m-%d")
-                        out['price'] = flight['dates'][0]['flights'][0]['regularFare']['fares'][0]['amount']
+                        out['depart'] = flight['origin']
+                        out['departLocation'] = flight['originName']
+                        out['destination'] = flight['destination']
+                        out['destinationLocation'] = flight['destinationName']
+                        flight_data = flight['dates'][0]['flights'][0]
+                        out['departure_time'] = datetime.fromisoformat(
+                            flight_data['time'][0]).strftime("%Y-%m-%d %H:%M:%S")
+                        out['arrival_time'] = datetime.fromisoformat(
+                            flight_data['time'][1]).strftime("%Y-%m-%d %H:%M:%S")
+                        out['duration'] = flight_data['duration']
+                        out['flightKey'] = flight_data['flightKey']
+                        out['flightNumber'] = flight_data['flightNumber']
+                        out['price'] = flight_data['regularFare']['fares'][0]['amount']
+                        out['faresLeft'] = flight_data['faresLeft']
+                        # aantal stops - segments tellen?
                         flights.append(out)
+
                     except:
                         print('iets ging miss')
-    
-    return(flights)
 
-
-
+    return (flights)
