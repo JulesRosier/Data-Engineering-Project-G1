29bc20a1c875ee2ba9d5f810af94fb771d870f8c
WardDeDeckere <ward.dedeckere@student.hogent.be>
2023-03-17 12:38:33 +0100

    Update: gegevens OK, captcha NOK

--- src/Transavia_Scrape.py
+++ src/Transavia_Scrape.py
@@ -10,12 +10,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time as t
 import datetime
-from fake_useragent import UserAgent
+dict = {}
+#from fake_useragent import UserAgent
 
 URL = 'https://www.transavia.com/nl-BE/boek-een-vlucht/uitgebreid-zoeken/zoeken/'
 DEPART = "Brussel, België"
-ARRIVAL = "Alicante, Spanje"
-MAAND = "april 2023"
+ARRIVAL = ["Alicante, Spanje", "Corfu, Griekenland", "Kreta (Heraklion), Griekenland", "Rhodos, Griekenland", "Brindisi, Italië", "Napels, Italië", "Palermo, Italië", "Faro, Portugal", "Ibiza, Spanje", "Malaga, Spanje", "Palma, Spanje", "Tenerife, Spanje"]
+CODEARRIVAL = ["ALC", "CFU", "HER", "RHO", "BDS", "NAP", "PMO", "FAO", "IBZ", "AGP", "PMI", "TFS"]
+MAAND = ["april 2023", "mei 2023", "juni 2023", "juli 2023", "augustus 2023", "september 2023", "oktober 2023"]
 
 def get_driver():
     chrome_options = Options()
@@ -40,11 +42,14 @@ def get_data():
     wait = WebDriverWait(driver, 10)
 
     #2.1 uitgebreid zoeken: accepteer cookies
-
-    cookies = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='cb__button cb__button--accept-all']")))
-    cookies.click()
-
-    #2.2 voer in: vanaf Brussel, België
+    try:
+        cookies = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='cb__button cb__button--accept-all']")))
+        cookies.click()
+    except:
+        pass
+    #reload voor captcha
+    t.sleep(20)
+    # #2.2 voer in: vanaf Brussel, België
     
     inputDepartElement = driver.find_element(By.ID, "countryStationSelection_Origin-input")
     t.sleep(4)
@@ -54,11 +59,31 @@ def get_data():
     #inputElement.send_keys(Keys.ENTER) #enter simuleren
 
     #2.3 voer in: naar bestemming, vb Alicante, Spanje --> niet meer nodig, check komt later obv div
+    t.sleep(10)
+    inputArriveElement = driver.find_element(By.ID, "countryStationSelection_Destination-input")
+    t.sleep(6)
+    # for i in range(ARRIVAL):
+    #     inputArriveElement.send_keys(ARRIVAL[i])
 
-    # inputArriveElement = driver.find_element(By.ID, "countryStationSelection_Destination-input")
-    # t.sleep(6)
-    # inputArriveElement.send_keys(ARRIVAL)
-    #inputElement.send_keys(Keys.ENTER) #enter simuleren
+    
+    # s = Select(driver.find_element(By.XPATH, "//*[@id='countryStationSelection_Destination']"))
+    # t.sleep(3)
+    # bestemmingen = [x.text for x in s.options]
+    # t.sleep(3)
+    # print(bestemmingen)
+    # t.sleep(50)
+
+    #klik op meerdere bestemmingen
+    # meerdereBestemmingen = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div/div[2]/div/div[1]/div/div/form/div[2]/div[2]/div/div[2]/div/button')))
+    # meerdereBestemmingen.click()
+
+    # for bestemming in ARRIVAL[1:]:
+    #     inputArriveElement = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/div[2]/div/div[1]/div/div/form/div[2]/div[2]/div/div[2]/div/button')
+    #     t.sleep(3)
+    #     inputArriveElement.send_keys(bestemming)
+    #     meerdereBestemmingen = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='link no-underline']")))
+    #     meerdereBestemmingen.click()
+    
 
     #2.4 klik op "weet je al wanneer je wil vertrekken?"
     t.sleep(3)
@@ -72,23 +97,37 @@ def get_data():
     selectEnkeleVlucht.select_by_visible_text('Enkele vlucht')
 
     #2.6 kies maand
-    t.sleep(4)
-    selectMaand = Select(driver.find_element(By.ID, "timeFrameSelection_SingleFlight_SpecificMonth"))
-    selectMaand.select_by_visible_text(MAAND)
+    # t.sleep(4)
+    # selectMaand = Select(driver.find_element(By.ID, "timeFrameSelection_SingleFlight_SpecificMonth"))
+    # selectMaand.select_by_visible_text(MAAND)
 
 
     #2.7 klik op zoeken
-    #Probleem captcha: zoeken geeft geen data indien geen captcha gedaan, oplossen 
-    knopZoeken = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='button button-primary']")))
-    knopZoeken.click()
+    #zoek per maand en per arrival
+    #Probleem captcha: zoeken geeft geen data indien geen captcha gedaan, oplossen
+    for maand in MAAND:
+        t.sleep(3)
+        selectMaand = Select(driver.find_element(By.ID, "timeFrameSelection_SingleFlight_SpecificMonth"))
+        selectMaand.select_by_visible_text(maand)
+        for dest in ARRIVAL:
+            t.sleep(3)
+            inputArriveElement.send_keys(dest)
+            try:
+                knopZoeken = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@class='button button-primary']")))
+                knopZoeken.click()
+            
+            
 
     #2.8 check of bestemming in available bestemmingen ligt. zo ja, klik op toon resultaten
 
     #2.8.1 check of div ID juist is --> Alicante = <div id="ALC"
-
-    Bestemming = "ALC"
-    knopResultaten = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="{}"]/button[1]'.format(Bestemming))))
-    knopResultaten.click()
+                t.sleep(3)
+                index = ARRIVAL.index(dest)
+                code = CODEARRIVAL[index]
+                knopResultaten = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="{}"]/button[1]'.format(code))))
+                knopResultaten.click()
+                t.sleep(3)
+            
 
     #2.9 klik op iedere kaart, haal datum, vertrek en prijs op
     
@@ -97,12 +136,15 @@ def get_data():
     # for div in divs:
     #     div.click()
     #     t.sleep(3)
+            #dict: key per maand, values per datum en prijs
+                for datum in driver.find_elements(By.XPATH, ".//span[@class='is-hidden is-visible-inline-block--bp20']"):
+                    print(datum.text)
 
-    for datum in driver.find_elements(By.XPATH, ".//span[@class='is-hidden is-visible-inline-block--bp20']"):
-        print(datum.text)
-
-    for prijs in driver.find_elements(By.XPATH, ".//div[@class='HV-gu--bp0--x2-2 HV-gu--bp0--50p text-align-right']//span[@class='integer']"):
-        print(prijs.text)
+                for prijs in driver.find_elements(By.XPATH, ".//div[@class='HV-gu--bp0--x2-2 HV-gu--bp0--50p text-align-right']//span[@class='integer']"):
+                    print(prijs.text)
+            except:
+                pass
+            
 
     
     
@@ -138,4 +180,4 @@ def solve_captcha():
     webbie = WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframe[name^='a-'][src^='https://www.google.com/recaptcha/api2/anchor?']")))
     wobbie = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@id='recaptcha-anchor']"))).click()
 
-call_site()
+get_data()
