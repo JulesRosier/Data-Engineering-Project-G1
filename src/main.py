91d8b59791a0f777ca994c7512966225cfa03bf0
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-03-29 16:05:30 +0200

    main met argument

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import Ryanair_Scrape
 import Brussels_Airlines
 from repository import connect_db, close_db, seed_db
 import time
+import sys
 
 from pprint import pprint
 
@@ -14,6 +15,11 @@ NUMBER_OF_DAYS = 10
 
 # zorgt er voor dat de code alleen maar loopt als je expliciet main.py uitvoert
 if __name__ == "__main__": 
+    if len(sys.argv) != 2:
+        print("Usage: main.py <script>")
+        sys.exit(1)
+    script = sys.argv[1]
+    
     print(f"Running script at {datetime.datetime.now()}")
     dates = get_dates(NUMBER_OF_DAYS)
     # time.sleep(5)
@@ -27,10 +33,14 @@ if __name__ == "__main__":
     connect_db()
     seed_db()
 
-    pprint(TuiFly_Scrape.get_data(ARIVE, dates))
+    if script == "TuiFly_Scrape":
+        # pprint(TuiFly_Scrape.get_data(ARIVE, dates))
+        TuiFly_Scrape.get_data(ARIVE, dates)
+        
+    if script == "Ryanair_Scrape":
+        Ryanair_Scrape.get_data(ARIVE, dates)
 
-    # Ryanair_Scrape.get_data(ARIVE, dates)
-
-    # pprint(Brussels_Airlines.get_data(ARIVE, dates))
+    # if script == "Brussels_Airlines":
+        # Brussels_Airlines.get_data(ARIVE, dates)
 
     close_db()
