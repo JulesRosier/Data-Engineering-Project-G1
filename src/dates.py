dce058b639ee879597a16a18d2859fdf7178e831
JulesRosier <julesrosier@student.hogent.be>
2023-03-31 18:49:50 +0200

    kleine fixxen

--- src/dates.py
+++ src/dates.py
@@ -1,9 +1,22 @@
-import datetime
+# import datetime
 
-def get_dates(num_of_days):
-    today = datetime.date.today()
-    # today = datetime.timedelta(days=10) + today
-    dates = []
-    for i in range(num_of_days):
-        dates.append(datetime.timedelta(days=i) + today)
-    return dates
+# def get_dates(num_of_days):
+#     today = datetime.date.today()
+#     dates = []
+#     for i in range(num_of_days):
+#         dates.append(datetime.timedelta(days=i) + today)
+#     return dates
+
+from datetime import datetime, timedelta
+
+def get_dates(end_date):
+    """Generator that yields all days from today until the given end date."""
+    current_date = datetime.now().date()
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+
+    out = []
+    while current_date <= end_date:
+        out.append(current_date)
+        current_date += timedelta(days=1)
+
+    return out
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+import datetime
 from dates import get_dates
 import TuiFly_Scrape
 import Ryanair_Scrape
@@ -11,7 +12,8 @@ import sys
 from pprint import pprint
 
 ARIVE = ['AGP', 'CFU', 'HER', 'RHO', 'BDS', 'NAP', 'PMO', 'FAO', 'ALC', 'IBZ', 'PMI', 'TFS']
-NUMBER_OF_DAYS = 185 # tot en met 1 oktober
+# NUMBER_OF_DAYS = 185 # tot en met 1 oktober
+END_DATE = '2023-10-01'
 
 # zorgt er voor dat de code alleen maar loopt als je expliciet main.py uitvoert
 if __name__ == "__main__": 
@@ -21,8 +23,7 @@ if __name__ == "__main__":
     script = sys.argv[1]
     
     print(f"Running script at {datetime.datetime.now()}")
-    dates = get_dates(NUMBER_OF_DAYS)
-    # time.sleep(5)
+    dates = get_dates(END_DATE)
 
     res = requests.get('https://kernel.org/')
     if (res.status_code):
@@ -33,6 +34,8 @@ if __name__ == "__main__":
     connect_db()
     seed_db()
 
+    start_time = time.time()
+
     if script == "tuifly":
         TuiFly_Scrape.get_data(ARIVE, dates)
         
@@ -42,10 +45,8 @@ if __name__ == "__main__":
     if script == "brussels":
         Brussels_Airlines.get_data(ARIVE, dates)
     
-    print("Geen juist script op gegeven")
-    print("opties zijn de volgende:")
-    print(" tuifly")
-    print(" ryanair")
-    print(" brussels")
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
+    print(f"Elapsed time: {elapsed_time} seconds")
     close_db()
