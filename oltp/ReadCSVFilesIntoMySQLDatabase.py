e008e2770129ca4020be73899c382913265eb256
WardDeDeckere <ward.dedeckere@student.hogent.be>
2023-04-13 22:16:33 +0200

    Problem fix (hopelijk)

--- oltp/ReadCSVFilesIntoMySQLDatabase.py
+++ oltp/ReadCSVFilesIntoMySQLDatabase.py
@@ -14,51 +14,51 @@ DB_HOST = os.getenv('DB_HOST')
 DB_DATABASE = 'flight_oltp'
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DOWNLOADS_FOLDER = os.getenv('REPO_PATH')
+DOWNLOADS_FOLDER = os.getenv('REPO_PATH') #= csv folder path
+OLTP_FOLDER = os.getenv('OLTP_PATH') 
 
 try:
     # autocommit is zéér belangrijk.
-    conn = mysql.connect(host=DB_HOST, database=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, autocommit=True)    
+    conn = mysql.connect(host=DB_HOST, database=DB_DATABASE, user=DB_USER, password=DB_PASSWORD, autocommit=True)
     print(type(conn))
     if conn.is_connected():
-        # choose a specific start_date    
+        # choose a specific start_date
         start_date = date(2023, 4, 6)
         end_date = date.today()
-        delta = timedelta(days=1) 
+        delta = timedelta(days=1)
         while start_date <= end_date:
 
             date_format = start_date.strftime("%Y_%m_%d")
 
             # for each date check if the file exists and copy the file to C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\
             old_path = DOWNLOADS_FOLDER + "All" + "_" + date_format + ".csv"
-            new_path = "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\" + "All" + ".csv"
-            
+            new_path = "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\" + "All" + "_" + date_format + ".csv"
+
             # Remove file if it already exists
             if os.path.exists(new_path):
                 os.remove(new_path)
-            if os.path.exists(old_path):    
+            if os.path.exists(old_path):
                 shutil.copy(old_path, new_path)
 
             # conn.reconnect is important, otherwise error
             conn.reconnect()
-            cursor = conn.cursor()  
+            cursor = conn.cursor()
 
             # Execute commands in file LoadFiles.sql to import data into database airfares
             file_name = 'LoadFiles.sql'
-            
-            for root, dirs, files in os.walk('/'):
-                if file_name in files:
-                    path = os.path.join(root, file_name)
-                    break
-            
-            with open(path, 'r') as file:
-                cursor.execute(file.read(), multi=True)   
-                                           
+
+            # for root, dirs, files in os.walk('/'):
+            #     if file_name in files:
+            #         path = os.path.join(root, file_name)
+            #         break
+
+            with open('{}{}'.format(OLTP_FOLDER, file_name), 'r') as f:
+                cursor.execute(f.read(), multi=True)    
             cursor.close()
 
             start_date += delta
-            
-    conn.close()       
+
+    conn.close()
 
 except Error as e:
     print("Error while connecting to MySQL", e)
