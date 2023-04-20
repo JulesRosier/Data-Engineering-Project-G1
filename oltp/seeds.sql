0b667b3d6e2a5c00c047fdd018fcf9b2f73af8aa
JulesRosier <julesrosier@student.hogent.be>
2023-04-20 10:46:29 +0200

    nullen weg gedaan

--- oltp/seeds.sql
+++ oltp/seeds.sql
@@ -1,53 +1,53 @@
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Brussels South Charleroi Airport', `city` = 'Charleroi', `country` = 'Belgium', `distance_flown` = 0
+SET `name` = 'Brussels South Charleroi Airport', `city` = 'Charleroi', `country` = 'Belgium'
 WHERE `iata` = 'CRL';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Brussels Airport', `city` = 'Brussels', `country` = 'Belgium', `distance_flown` = 0
+SET `name` = 'Brussels Airport', `city` = 'Brussels', `country` = 'Belgium'
 WHERE `iata` = 'BRU';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Antwerp International Airport', `city` = 'Antwerp', `country` = 'Belgium', `distance_flown` = 0
+SET `name` = 'Antwerp International Airport', `city` = 'Antwerp', `country` = 'Belgium'
 WHERE `iata` = 'ANR';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Liège Airport', `city` = 'Liège', `country` = 'Belgium', `distance_flown` = 0
+SET `name` = 'Liège Airport', `city` = 'Liège', `country` = 'Belgium'
 WHERE `iata` = 'LGG';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Ostend–Bruges International Airport', `city` = 'Ostend', `country` = 'Belgium', `distance_flown` = 0
+SET `name` = 'Ostend–Bruges International Airport', `city` = 'Ostend', `country` = 'Belgium'
 WHERE `iata` = 'OST';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Corfu International Airport', `city` = 'Corfu', `country` = 'Greece', `distance_flown` = 0
+SET `name` = 'Corfu International Airport', `city` = 'Corfu', `country` = 'Greece'
 WHERE `iata` = 'CFU';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Brindisi Airport', `city` = 'Brindisi', `country` = 'Italy', `distance_flown` = 0
+SET `name` = 'Brindisi Airport', `city` = 'Brindisi', `country` = 'Italy'
 WHERE `iata` = 'BDS';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Naples International Airport', `city` = 'Naples', `country` = 'Italy', `distance_flown` = 0
+SET `name` = 'Naples International Airport', `city` = 'Naples', `country` = 'Italy'
 WHERE `iata` = 'NAP';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Alicante–Elche Airport', `city` = 'Alicante', `country` = 'Spain', `distance_flown` = 0
+SET `name` = 'Alicante–Elche Airport', `city` = 'Alicante', `country` = 'Spain'
 WHERE `iata` = 'ALC';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Rhodes International Airport', `city` = 'Rhodes', `country` = 'Greece', `distance_flown` = 0
+SET `name` = 'Rhodes International Airport', `city` = 'Rhodes', `country` = 'Greece'
 WHERE `iata` = 'RHO';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Málaga Airport', `city` = 'Málaga', `country` = 'Spain', `distance_flown` = 0
+SET `name` = 'Málaga Airport', `city` = 'Málaga', `country` = 'Spain'
 WHERE `iata` = 'AGP';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Palma de Mallorca Airport', `city` = 'Palma de Mallorca', `country` = 'Spain', `distance_flown` = 0
+SET `name` = 'Palma de Mallorca Airport', `city` = 'Palma de Mallorca', `country` = 'Spain'
 WHERE `iata` = 'PMI';
 
 UPDATE `flight_oltp`.`flight_airport`
-SET `name` = 'Heraklion International Airport', `city` = 'Heraklion', `country` = 'Greece', `distance_flown` = 0
+SET `name` = 'Heraklion International Airport', `city` = 'Heraklion', `country` = 'Greece'
 WHERE `iata` = 'HER';
 
 UPDATE `flight_oltp`.`flight_airport`
