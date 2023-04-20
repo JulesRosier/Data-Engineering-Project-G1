3ebdc410d0dd40c2ff5977434181d3336bd72f11
JulesRosier <julesrosier@student.hogent.be>
2023-04-20 10:43:51 +0200

    seeding verandert naar updates ipv insert

--- oltp/seeds.sql
+++ oltp/seeds.sql
@@ -1,27 +1,111 @@
-# Airport
-INSERT INTO `flight_oltp`.`flight_airport` (`iata`, `name`, `city`, `country`)
-VALUES 
-('CRL', 'Brussels South Charleroi Airport', 'Charleroi', 'Belgium'),
-('BRU', 'Brussels Airport', 'Brussels', 'Belgium'),
-('ANR', 'Antwerp International Airport', 'Antwerp', 'Belgium'),
-('LGG', 'Liège Airport', 'Liège', 'Belgium'),
-('OST', 'Ostend–Bruges International Airport', 'Ostend', 'Belgium'),
-('CFU', 'Corfu International Airport', 'Corfu', 'Greece'),
-('BDS', 'Brindisi Airport', 'Brindisi', 'Italy'),
-('NAP', 'Naples International Airport', 'Naples', 'Italy'),
-('ALC', 'Alicante–Elche Airport', 'Alicante', 'Spain'),
-('RHO', 'Rhodes International Airport', 'Rhodes', 'Greece'),
-('AGP', 'Málaga Airport', 'Málaga', 'Spain'),
-('PMI', 'Palma de Mallorca Airport', 'Palma de Mallorca', 'Spain'),
-('HER', 'Heraklion International Airport', 'Heraklion', 'Greece'),
-('PMO', 'Falcone–Borsellino Airport', 'Palermo', 'Italy'),
-('FAO', 'Faro Airport', 'Faro', 'Portugal'),
-('TFS', 'Tenerife South Airport', 'Tenerife', 'Spain'),
-('IBZ', 'Ibiza Airport', 'Ibiza', 'Spain');
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Brussels South Charleroi Airport', `city` = 'Charleroi', `country` = 'Belgium', `distance_flown` = 0
+WHERE `iata` = 'CRL';
 
-# Airline
-INSERT INTO `flight_oltp`.`flight_airline` (`iata`, `name`) VALUES
-('FR', 'Ryanair'),
-('HV', 'Transavia'),
-('TB', 'TUI fly'),
-('SN', 'Brussels Airlines');
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Brussels Airport', `city` = 'Brussels', `country` = 'Belgium', `distance_flown` = 0
+WHERE `iata` = 'BRU';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Antwerp International Airport', `city` = 'Antwerp', `country` = 'Belgium', `distance_flown` = 0
+WHERE `iata` = 'ANR';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Liège Airport', `city` = 'Liège', `country` = 'Belgium', `distance_flown` = 0
+WHERE `iata` = 'LGG';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Ostend–Bruges International Airport', `city` = 'Ostend', `country` = 'Belgium', `distance_flown` = 0
+WHERE `iata` = 'OST';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Corfu International Airport', `city` = 'Corfu', `country` = 'Greece', `distance_flown` = 0
+WHERE `iata` = 'CFU';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Brindisi Airport', `city` = 'Brindisi', `country` = 'Italy', `distance_flown` = 0
+WHERE `iata` = 'BDS';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Naples International Airport', `city` = 'Naples', `country` = 'Italy', `distance_flown` = 0
+WHERE `iata` = 'NAP';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Alicante–Elche Airport', `city` = 'Alicante', `country` = 'Spain', `distance_flown` = 0
+WHERE `iata` = 'ALC';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Rhodes International Airport', `city` = 'Rhodes', `country` = 'Greece', `distance_flown` = 0
+WHERE `iata` = 'RHO';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Málaga Airport', `city` = 'Málaga', `country` = 'Spain', `distance_flown` = 0
+WHERE `iata` = 'AGP';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Palma de Mallorca Airport', `city` = 'Palma de Mallorca', `country` = 'Spain', `distance_flown` = 0
+WHERE `iata` = 'PMI';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Heraklion International Airport', `city` = 'Heraklion', `country` = 'Greece', `distance_flown` = 0
+WHERE `iata` = 'HER';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Antwerp International Airport', `city` = 'Antwerp', `country` = 'Belgium'
+WHERE `iata` = 'ANR';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Liège Airport', `city` = 'Liège', `country` = 'Belgium'
+WHERE `iata` = 'LGG';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Ostend–Bruges International Airport', `city` = 'Ostend', `country` = 'Belgium'
+WHERE `iata` = 'OST';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Corfu International Airport', `city` = 'Corfu', `country` = 'Greece'
+WHERE `iata` = 'CFU';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Brindisi Airport', `city` = 'Brindisi', `country` = 'Italy'
+WHERE `iata` = 'BDS';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Naples International Airport', `city` = 'Naples', `country` = 'Italy'
+WHERE `iata` = 'NAP';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Alicante–Elche Airport', `city` = 'Alicante', `country` = 'Spain'
+WHERE `iata` = 'ALC';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Rhodes International Airport', `city` = 'Rhodes', `country` = 'Greece'
+WHERE `iata` = 'RHO';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Málaga Airport', `city` = 'Málaga', `country` = 'Spain'
+WHERE `iata` = 'AGP';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Palma de Mallorca Airport', `city` = 'Palma de Mallorca', `country` = 'Spain'
+WHERE `iata` = 'PMI';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Heraklion International Airport', `city` = 'Heraklion', `country` = 'Greece'
+WHERE `iata` = 'HER';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Falcone–Borsellino Airport', `city` = 'Palermo', `country` = 'Italy'
+WHERE `iata` = 'PMO';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Faro Airport', `city` = 'Faro', `country` = 'Portugal'
+WHERE `iata` = 'FAO';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Tenerife South Airport', `city` = 'Tenerife', `country` = 'Spain'
+WHERE `iata` = 'TFS';
+
+UPDATE `flight_oltp`.`flight_airport`
+SET `name` = 'Ibiza Airport', `city` = 'Ibiza', `country` = 'Spain'
+WHERE `iata` = 'IBZ';
