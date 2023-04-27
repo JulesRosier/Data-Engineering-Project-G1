1b609c0cb92762737b23d6bd19b36f3ce3932db4
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-04-27 11:01:20 +0200

    tables airplane en airport distance weg

--- oltp/oltp.sql
+++ oltp/oltp.sql
@@ -16,16 +16,6 @@ CREATE TABLE IF NOT EXISTS flight_oltp.flight_airport(
     PRIMARY KEY (iata)
 );
 
--- Flight Airport Distance
-CREATE TABLE IF NOT EXISTS flight_oltp.flight_airport_distance(
-    airport1_iata CHAR(3) NOT NULL,
-    airport2_iata CHAR(3) NOT NULL,
-    distance_flown INT,
-    PRIMARY KEY (airport1_iata, airport2_iata),
-    FOREIGN KEY (airport1_iata) REFERENCES flight_oltp.flight_airport (iata),
-    FOREIGN KEY (airport2_iata) REFERENCES flight_oltp.flight_airport (iata)
-);
-
 -- Airline
 CREATE TABLE IF NOT EXISTS flight_oltp.flight_airline(
     iata CHAR(3) UNIQUE NOT NULL,
@@ -65,16 +55,6 @@ CREATE TABLE IF NOT EXISTS flight_oltp.flight_var_data (
   FOREIGN KEY (flight_key) REFERENCES flight_oltp.flight_fixed_data (flight_key)
 );
 
-  -- Airplane
-CREATE TABLE IF NOT EXISTS flight_oltp.flight_airplane(
-    airplane_type VARCHAR(30),
-    airplane_age INT,
-    total_seats INT,
-    flight_number VARCHAR(10) NOT NULL,
-    PRIMARY KEY (airplane_type),
-    FOREIGN KEY (flight_number) REFERENCES flight_oltp.flight_fixed_data (flight_number)
-);
-
 -- 3) Data inladen
 -- Airport 
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\All.csv'
@@ -93,7 +73,6 @@ LINES TERMINATED BY '\n'
 (@flight_id, @flight_number, @departure_date, @arrival_date, @departure_time, @arrival_time, @duration, @number_of_stops, @airline_iata_code, @departure_airport_iata_code, @arrival_airport_iata_code, @scrape_date, @available_seats, @price)
 SET iata = @arrival_airport_iata_code;
 
-
 -- Airline 
 LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\All.csv"
 IGNORE INTO TABLE flight_oltp.flight_airline
@@ -102,7 +81,7 @@ LINES TERMINATED BY "\n"
 (@flight_id, @flight_number, @departure_date, @arrival_date, @departure_time, @arrival_time, @duration, @number_of_stops, @airline_iata_code, @departure_airport_iata_code, @arrival_airport_iata_code, @scrape_date, @available_seats, @price)
 SET iata=@airline_iata_code;
 
--- Fixed Flight_Data 
+-- Fixed Flight Data 
 LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\All.csv"
 IGNORE INTO TABLE flight_oltp.flight_fixed_data
 FIELDS TERMINATED BY ","
@@ -110,21 +89,13 @@ LINES TERMINATED BY "\n"
 (@flight_id, @flight_number, @departure_date, @arrival_date, @departure_time, @arrival_time, @duration, @number_of_stops, @airline_iata_code, @departure_airport_iata_code, @arrival_airport_iata_code, @scrape_date, @available_seats, @price)
 SET flight_key=@flight_id, flight_number=@flight_number, number_of_stops=@number_of_stops, departure_date=@departure_date, arrival_date=@arrival_date, departure_time=@departure_time, arrival_time=@arrival_time, duration=@duration, departure_airport=@departure_airport_iata_code, arrival_airport=@arrival_airport_iata_code, operating_airline=@airline_iata_code;
 
--- Var Flight_Data 
+-- Var Flight Data 
 LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\All.csv"
 IGNORE INTO TABLE flight_oltp.flight_var_data
 FIELDS TERMINATED BY ","
 LINES TERMINATED BY "\n"
 (@flight_id, @flight_number, @departure_date, @arrival_date, @departure_time, @arrival_time, @duration, @number_of_stops, @airline_iata_code, @departure_airport_iata_code, @arrival_airport_iata_code, @scrape_date, @available_seats, @price)
 SET scrape_date=@scrape_date, price=@price, seats_available=@available_seats, flight_key=@flight_id;
-
--- Airplane
-LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\LoadInfo.csv"
-IGNORE INTO TABLE flight_oltp.flight_airplane
-FIELDS TERMINATED BY ","
-LINES TERMINATED BY "\n"
-(@flight_number, @airplane_type, @airplane_age, @total_seats, @distance_flown)
-SET flight_number=@flight_number, airplane_type=@airplane_type, airplane_age=@airplane_age, total_seats=@total_seats;
 
 -- Alle instellingen terug op standaardinstellingen plaatsen.
 SET GLOBAL local_infile = "OFF";
@@ -208,19 +179,9 @@ SET name = 'Transavia'
 WHERE iata = 'HV';
 
 UPDATE flight_oltp.flight_airline
-SET name = 'TUI fly'
+SET name = 'TUIfly'
 WHERE iata = 'TB';
 
 UPDATE flight_oltp.flight_airline
 SET name = 'Brussels Airlines'
 WHERE iata = 'SN';
-
--- Flight Airport Distance
-LOAD DATA INFILE "C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\LoadInfo.csv"
-IGNORE INTO TABLE flight_oltp.flight_airport_distance
-FIELDS TERMINATED BY ","
-LINES TERMINATED BY "\n"
-(@flight_number, @airplane_type, @airplane_age, @total_seats, @distance_flown)
-SET distance_flown=@distance_flown,
-airport1_iata=(select departure_airport from flight_fixed_data where flight_number = @flight_number LIMIT 1),
-airport2_iata=(select arrival_airport from flight_fixed_data where flight_number = @flight_number LIMIT 1);
